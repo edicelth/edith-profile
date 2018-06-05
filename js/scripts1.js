7f3542e73c931043e61a8778bd8eacf3ed334720
edicelth <edicelth@hotmail.com>
2018-06-05 11:49:01 -0700

    more changes

--- js/scripts1.js
+++ js/scripts1.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   $("#button1").click(function(){
 
-    var loan =  parseInt($("#loam").val());
+    var loan =  parseInt(   $("#loam").val()   );
     var interes = parseInt($("#interes").val()) / 100.0  / 12.0;
     var payments = parseInt($("#time").val()) * 12.0;
 
@@ -10,5 +10,6 @@ $(document).ready(function() {
     var monthly_payment = ((loan) * rate * interes) / (rate - 1);
 
       $("#mounthPayment").val(Math.round(monthly_payment));
+      $("#payment").text("The mounthly payment is $" + Math.round(monthly_payment) + ".");
   });
 });
